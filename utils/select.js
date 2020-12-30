ba338b0e21f7252c9671ebad85f2317186e3614c
Christos Lytras <christos.lytras@gmail.com>
2020-12-30 18:28:14 +0200

    add(select): switchOf function

--- utils/select.js
+++ utils/select.js
@@ -1,12 +1,12 @@
 
 function _select(check, cases) {
-  if(!check || !cases) {
+  if (!check || !cases) {
     throw new Error('select: No valid input params');
   }
 
   let selected;
 
-  if(check in cases) {
+  if (check in cases) {
     selected = cases[check];
   } else if('default' in cases) {
     selected = cases['default'];
@@ -18,7 +18,7 @@ function _select(check, cases) {
 export default function(check, cases) {
   const selected = _select(check, cases);
 
-  if(typeof(selected) === 'function') {
+  if (typeof(selected) === 'function') {
     return selected();
   }
 
@@ -28,9 +28,23 @@ export default function(check, cases) {
 export async function select(check, cases) {
   const selected = _select(check, cases);
 
-  if(typeof(selected) === 'function') {
+  if (typeof(selected) === 'function') {
     return await selected();
   }
 
   return selected;
+}
+
+// Uses arrays to evaluate expressions and return n+1 for truthy values
+// ex.:
+// > switchOf([1 > 0.1, 'one', 2 > 22, 'two'], 'default')
+// < "one"
+export function switchOf(flagCases, defaultValue) {
+  for (let i = 0; i < flagCases.length; i += 2) {
+    const flag = flagCases[i];
+    if (flag) {
+      return flagCases[i + 1];
+    }
+  }
+  return defaultValue;
 }
